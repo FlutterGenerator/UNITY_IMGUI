b80095be83460f23a5ba3f317dd6059d72c48ddd
tojik_proof_93 <elementman50@yandex.com>
2026-01-27 23:42:30 +0500

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,11 +2,11 @@
 APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-18 #APP_PLATFORM does not need to be set. It will automatically defaulting
 APP_STL := c++_static
-APP_OPTIM := release
+APP_OPTIMA := release
 APP_THIN_ARCHIVE := true
-APP_PIE 		:= true
+APP_PIE := true
 APP_CPPFLAGS += -frtti
-
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 ifneq ($(APP_OPTIM), debug)
   $(info APP_OPTIM is $(APP_OPTIM) ...)
@@ -14,4 +14,3 @@ ifneq ($(APP_OPTIM), debug)
   APP_CFLAGS  += -fvisibility=hidden -fvisibility-inlines-hidden
   APP_CFLAGS  += -g0 -O3 -fomit-frame-pointer -ffunction-sections -fdata-sections
 endif
-
