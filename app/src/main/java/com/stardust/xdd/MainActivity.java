317388fd8463fd786246206eaf72d0f31eb13823
tojik_proof_93 <elementman50@yandex.com>
2026-01-28 23:26:31 +0500

    Update MainActivity.java

--- app/src/main/java/com/stardust/xdd/MainActivity.java
+++ app/src/main/java/com/stardust/xdd/MainActivity.java
@@ -5,16 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-  static {
-    // When you change the lib name, change also on Android.mk file
-    // Both must have same name
-    System.loadLibrary("stardust");
-  }
+    static {
+        // When you change the lib name, change also on Android.mk file
+        // Both must have same name
+        System.loadLibrary("stardust");
+    }
 
-  // To call onCreate, please refer to README.md
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    // To call onCreate, please refer to README.md
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
