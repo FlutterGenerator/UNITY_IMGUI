3d39d0b3ec275ad9b056814906db9ea0d8267f06
tojik_proof_93 <elementman50@yandex.com>
2026-01-29 00:35:10 +0500

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -5,7 +5,6 @@ APP_STL := c++_static
 APP_OPTIMA := release
 APP_THIN_ARCHIVE := true
 APP_PIE := true
-APP_CPPFLAGS += -frtti
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 ifneq ($(APP_OPTIM), debug)
