2af0aa66fd2ca1d3f021d14363a498e19d5dd595
tojik_proof_93 <elementman50@yandex.com>
2026-01-29 01:13:18 +0500

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,9 +2,11 @@
 APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-18 #APP_PLATFORM does not need to be set. It will automatically defaulting
 APP_STL := c++_static
-APP_OPTIMA := release
+APP_OPTIM := release
 APP_THIN_ARCHIVE := true
-APP_PIE := true
+APP_PIE 		:= true
+
+
 
 ifneq ($(APP_OPTIM), debug)
   $(info APP_OPTIM is $(APP_OPTIM) ...)
@@ -12,3 +14,4 @@ ifneq ($(APP_OPTIM), debug)
   APP_CFLAGS  += -fvisibility=hidden -fvisibility-inlines-hidden
   APP_CFLAGS  += -g0 -O3 -fomit-frame-pointer -ffunction-sections -fdata-sections
 endif
+
