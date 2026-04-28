cebe4e85b5b36524bef7e7a06b3ada4d17ddb538
tojik_proof_93 <elementman50@yandex.com>
2026-04-29 03:18:52 +0500

    Update MainActivity.java

--- app/src/main/java/com/stardust/xdd/MainActivity.java
+++ app/src/main/java/com/stardust/xdd/MainActivity.java
@@ -2,19 +2,49 @@ package com.stardust.xdd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     static {
-        // When you change the lib name, change also on Android.mk file
-        // Both must have same name
         System.loadLibrary("stardust");
     }
 
-    // To call onCreate, please refer to README.md
+    // JNI методы для передачи тач-событий в C++
+    public static native void nativeTouchDown(float x, float y);
+    public static native void nativeTouchUp(float x, float y);
+    public static native void nativeTouchMove(float x, float y);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Вешаем touch listener на корневой View
+        View rootView = getWindow().getDecorView().getRootView();
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getRawX();
+                float y = event.getRawY();
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        nativeTouchDown(x, y);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_POINTER_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        nativeTouchUp(x, y);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        nativeTouchMove(x, y);
+                        break;
+                }
+                // Возвращаем false чтобы игра тоже получала события
+                return false;
+            }
+        });
     }
 }
